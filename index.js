1b0a2cc572e61779d94792a73c0f8a17f66f65a5
Artur Sudnik <artur.sudnik@gmail.com>
2018-11-29 21:08:05 +0100

    v1.0.0

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    let counter = 0,
+        chunk   = `this is some chunk of data`;
+
+    req.socket.setTimeout(1100);
+
+    console.log(`received http request, starting sending chunks in response`);
+
+    let interval = setInterval(() => {
+        console.log(`sending another chunk`);
+        res.write(`${chunk} generated at ${new Date()}`);
+        counter++;
+        if (counter === 2) {
+            console.log(`stopping sending chunks`);
+            console.log(`keeping connection opened for a while`);
+            setTimeout(() => {
+                // console.log(`closing socket`);
+                // res.socket.destroy();
+
+                console.log(`finishing response`);
+                res.end();
+            }, 5000);
+            clearInterval(interval);
+            // res.end();
+        }
+    }, 1000)
+});
+
+app.listen(3000);
