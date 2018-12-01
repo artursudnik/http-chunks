a81658d45c59eac4170af172c26fd88e9f66f85f
Artur Sudnik <artur.sudnik@gmail.com>
2018-12-01 13:43:28 +0100

    correct HTML structure

--- index.js
+++ index.js
@@ -15,12 +15,22 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
     res.header('Cache-Control', 'no-store');
     res.header('Content-Type', 'text/html');
-    res.send(`
-        <a href="/dropBeforeEndCacheable">broken cacheable</a></br>
-        <a href="/correctChunksCacheable">correct cacheable</a></br>
-        <a href="/dropBeforeEndNonCacheable">broken non-cacheable</a></br>
-        <a href="/correctChunksNonCacheable">correct non-cacheable</a>
-    `)
+
+    const pageHTML = `<!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <title>Chunked transfer encoding test cases</title>
+            </head>
+            <body>
+                <a href="/dropBeforeEndCacheable">broken cacheable</a></br>
+                <a href="/correctChunksCacheable">correct cacheable</a></br>
+                <a href="/dropBeforeEndNonCacheable">broken non-cacheable</a></br>
+                <a href="/correctChunksNonCacheable">correct non-cacheable</a>
+            </body>
+        </html>`;
+
+    res.send(pageHTML);
 });
 
 app.get('/dropBeforeEndCacheable', (req, res) => {
